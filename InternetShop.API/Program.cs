c3c288b08fd68131c7e8a9ca65efb62f819a92d8
correc1ed <ivanov.daniil@crm-integpro.ru>
2024-11-27 18:02:47 +0300

    Step2

--- InternetShop.API/Program.cs
+++ InternetShop.API/Program.cs
@@ -1,3 +1,18 @@
+using InternetShop.Infrastructure;
+using InternetShop.Infrastructure.Extensions.DependencyInjection;
+using InternetShop.Infrastructure.Services.Jwt;
+using InternetShop.UseCases.Commands.Basket.DeleteProductFromBasketById;
+using InternetShop.UseCases.Commands.Basket.PostAddProductToBasketById;
+using InternetShop.UseCases.Commands.Order.PostOrder;
+using InternetShop.UseCases.Commands.Order.PutOrderStatus;
+using InternetShop.UseCases.Commands.Product.PostProduct;
+using InternetShop.UseCases.Commands.Product.PutProduct;
+using InternetShop.UseCases.Commands.User.PostUserLogin;
+using InternetShop.UseCases.Commands.User.PostUserRegistration;
+using InternetShop.UseCases.Commands.User.PutUserProfile;
+using InternetShop.UseCases.Queries.Order.GetOrderInformation;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace InternetShop.API;
 
@@ -10,9 +25,33 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+        builder.Services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.RegisterServicesFromAssembly(typeof(DeleteProductFromBasketByIdCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PostAddProductToBasketByIdCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(GetOrderInformationQuery).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PostOrderCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PutOrderStatusCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PostProductCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PutProductCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PostUserLoginCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PostUserRegistrationCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(PutUserProfileCommand).Assembly);
+        });
+
         builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(nameof(JwtOptions)));
+
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddInternetShop();
+
+        builder.Services.AddDbContext<EfContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
 
@@ -23,8 +62,9 @@ public class Program
             app.UseSwaggerUI();
         }
 
-        app.UseAuthorization();
+         app.UseHttpsRedirection();
 
+          app.UseAuthorization();
 
         app.MapControllers();
 
